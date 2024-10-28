fe29ab4ea8e6b73c672b40e0a5b08e85d1b529dd
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-28 16:00:40 -0300

    Fixture/atividade/correção

--- codigo3.py
+++ codigo3.py
@@ -1,2 +1,8 @@
+import pytest
+
 def soma_dobro(numeros):
     return sum(x * 2 for x in numeros)
+
+@pytest.fixture
+def lista_simples():
+    return [1, 2, 3, 4, 5]
--- test_codigo3.py
+++ test_codigo3.py
@@ -1,7 +1,7 @@
 from codigo3 import *
 
-def test_soma_dobro():
-    assert soma_dobro([1, 2, 3, 4, 5]) == 30
+def test_soma_dobro(lista_simples):
+    assert soma_dobro(lista_simples) == 30
     assert soma_dobro([0, 0, 0]) == 0
     assert soma_dobro([]) == 0
     assert soma_dobro([10]) == 20
